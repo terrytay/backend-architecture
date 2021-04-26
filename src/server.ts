5eb78906643b3409f3f8c92af4340628e32d2011
Terry Tay <terry@resynctech.com>
2021-04-26 17:46:28 +0800

    Wire up repo, service in server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,30 +2,32 @@ import App from "./app";
 import Controller from "controller.interface";
 import {HealthController} from "./controllers/health";
 import {IDBClient} from "./db/client";
+import {IUserRepository, UserRepository} from "./repositories/user";
+import {IUserService, UserService} from "./services/user";
 
 interface IRepositories {
-
+  userRepository: IUserRepository
 }
 
 interface IServices {
-
+  userService: IUserService
 }
 
 function getRepositories(dbClient: IDBClient): IRepositories {
   return {
-    
+    userRepository: new UserRepository(dbClient)
   };
 }
 
 function getServices(repos: IRepositories): IServices {
   return {
-    
+    userService: new UserService(repos.userRepository)
   };
 }
 
 function getControllers(services: IServices): Controller[] {
   return [
-    new HealthController()
+    new HealthController(),
   ];
 }
 
